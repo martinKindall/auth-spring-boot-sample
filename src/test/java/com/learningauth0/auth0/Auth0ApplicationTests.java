539f4c930ecf335617dfa54545df6967694981a7
martinKindall <mart256@gmail.com>
2021-03-14 23:13:42 +0100

    Initial commit

---
+++ src/test/java/com/learningauth0/auth0/Auth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningauth0.auth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
